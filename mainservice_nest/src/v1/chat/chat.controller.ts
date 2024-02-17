e2f540c7dc62d84f57813ed3f6a0274df4016479
moein moeinnia <moein@pro.ai>
2024-02-17 13:17:20 +0330

    chore: rx working

--- mainservice_nest/src/v1/chat/chat.controller.ts
+++ mainservice_nest/src/v1/chat/chat.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post, Res } from '@nestjs/common';
+import { Controller, Post, Res, Sse } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { Response } from 'express';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Controller('chat')
 export class ChatController {
@@ -8,16 +10,77 @@ export class ChatController {
 
   @Post('stream')
   async generateResponseAsync(@Res() res: Response) {
-    this.chatServcie.callLCForStream('')
-    // this.chatServcie.callLCForStream('').subscribe(
-    //   (response) => response.data.pipe(res),
-    this.chatServcie.callLCForStream('');
-    // this.chatServcie.callLCForStream('').subscribe(
-    //   (response) => response,
-    //   (error) => {
-    //     res.status(500).send('Internal Server Error');
-    //   },
-    // );
+    res.setHeader('Content-Type', 'text/event-stream');
+
+    const chatObservable = await this.chatServcie.callLCForStream('');
+
+    chatObservable
+      .pipe(
+        map((value) => {
+          // Do any processing or transformation here
+          if (!value) return '';
+          const originalStr = value?.toString();
+          const secondLine = originalStr?.split('\n')?.[1];
+          const onlyVal = secondLine?.replace('data:', '');
+          return { data: { message: onlyVal } }; // For example, convert the value to a string
+        }),
+      )
+      .subscribe({
+        next: (value) => {
+          console.log(1111);
+
+          console.log('Received:', value);
+          res.write(value);
+        },
+        error: (error) => {
+          console.log(2222);
+
+          console.error('Error:', error);
+        },
+        complete: () => {
+          console.log(3333);
+
+          console.log('Stream completed');
+          res.end();
+        },
+      });
+
     return 123;
+  }
+
+  @Sse('sse')
+  async sse(): Promise<Observable<any>> {
+    const chatObservable = await this.chatServcie.callLCForStream('');
+    //return interval(1000).pipe(map((_) => ({ data: { hello: 'world' } })));
+    return chatObservable.pipe(
+      map((value) => {
+        // Do any processing or transformation here
+        if (!value) return '';
+        const originalStr = value?.toString();
+        const secondLine = originalStr?.split('\n')?.[1];
+        const onlyVal = secondLine?.replace('data:', '');
+        return { data: onlyVal }; // For example, convert the value to a string
+      }),
+    );
+
+    // # THIS CODE BLOCK IS USED FOR DEBUGGING
+
+    // .subscribe({
+    //   next: (value) => {
+    //     console.log(1111);
+
+    //     console.log('Received:', value);
+    //   },
+    //   error: (error) => {
+    //     console.log(2222);
+
+    //     console.error('Error:', error);
+    //   },
+    //   complete: () => {
+    //     console.log(3333);
+
+    //     console.log('Stream completed');
+    //   },
+    // });
   }
 }
--- mainservice_nest/src/v1/chat/chat.service.ts
+++ mainservice_nest/src/v1/chat/chat.service.ts
@@ -10,103 +10,38 @@ import axios from 'axios';
 export class ChatService {
   constructor(private readonly httpService: HttpService) {}
 
-  callLCForStream(prompt: string): Observable<AxiosResponse<any, any>> {
-    //https://www.npmjs.com/package/axios-observable
-    // Create an observable from the axios GET request
-    const dataObservable = from(
-      axios.post('http://localhost:8000/plotset/stream', {
-        input: 'how to create barchart?',
+  async callLCForStream(prompt: string): Promise<Observable<string>> {
+    const response = await fetch('http://localhost:8000/plotset/stream', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/event-stream',
+      },
+      body: JSON.stringify({
+        input: 'what is plotset?',
       }),
-    );
-
-    // Subscribe to the observable to handle the response or error
-    dataObservable.subscribe({
-      next(response) {
-        console.log(123, response.data); // Handle the response data
-      },
-      error(err) {
-        console.error(err); // Handle the error
-      },
-      complete: () => {
-        console.log('Stream completed.');
-      },
-    });
-    // const observable = this.httpService.post(
-    //   'http://localhost:8000/plotset/stream',
-    //   {
-    //     input: 'how to create barchart?',
-    //   },
-    //   { responseType: 'stream' },
-    // );
-
-    // observable.pipe(
-    //   map((response) => {
-    //     // Assuming the response is a text stream
-    //     console.log(response.data.toString());
-    //     return response.data.toString();
-    console.log(123, observable);
-
-    observable.subscribe({
-      //sum: '',
-      next(value) {
-        console.log('Adding: ' + value);
-        //this.sum = this.sum + value;
-      },
-      error() {
-        // We actually could just remove this method,
-        // since we do not really care about errors right now.
-      },
-      complete() {
-        console.log('Sum equals: ');
-      },
     });
 
-    // return observable.pipe(
-    //   map((axiosResponse: AxiosResponse) => {
-    //     console.log(axiosResponse.data);
-    //     return axiosResponse.data;
-    //   }),
-    // );
-    // observable.subscribe({
-    //   next: (data) => {
-    //     console.log('Received data:', data.data);
-    //   },
-    // //   error: (error) => {
-    // //     console.error('Error:', error);
-    // //   },
-    //   complete: () => {
-    //     console.log('Stream completed.');
-    //   },
-    // });
-    return dataObservable;
+    const reader = response.body
+      .pipeThrough(new TextDecoderStream())
+      .getReader();
 
-    // observable.forEach((value) => {
-    //   console.log(value);
-    //   console.log(123);
-    //   return value;
-    // });
-
-    // observable.pipe(
-    //   map((res) => {
-    //     console.log(res.data);
-    //     return res.data;
-    //   }),
-    // );
-
-    // observable
-    //   .pipe(
-    //     map((value) => {
-    //       console.log({ value });
-
-    //       return value;
-    //     }),
-    //   )
-    //   .subscribe((result) => {
-    //     console.log(result);
-    //   });
-
-    // observable.subscribe((x) => {
-    //   console.log(666, x);
-    // });
+    return new Observable((observer) => {
+      (async function readStream() {
+        try {
+          while (true) {
+            const { value, done } = await reader.read();
+            if (done) {
+              observer.complete();
+              break;
+            }
+            console.log('Fetch Received:', value);
+            observer.next(value);
+          }
+        } catch (error) {
+          observer.error(error);
+          console.error('Fetch stream error: ', error);
+        }
+      })();
+    });
   }
 }
