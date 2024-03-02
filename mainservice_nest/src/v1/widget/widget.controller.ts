997f0b384b51450ba8c9f62d29a1c0fb051bfb9b
moein moeinnia <moein@pro.ai>
2024-03-02 16:11:52 +0330

    chore: widget endpoints

--- mainservice_nest/src/v1/widget/widget.controller.ts
+++ mainservice_nest/src/v1/widget/widget.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { WidgetService } from './widget.service';
 
 @Controller('widget')
-export class WidgetController {}
+export class WidgetController {
+  constructor(private readonly widgetService: WidgetService) {}
+
+  @Get('config')
+  async getBotConfig({ userId, botId }: { userId: string; botId: string }) {
+    return this.widgetService.getBotConfigService({
+      userId,
+      botId,
+    });
+  }
+}
--- mainservice_nest/src/v1/widget/widget.service.ts
+++ mainservice_nest/src/v1/widget/widget.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
 
 @Injectable()
-export class WidgetService {}
+export class WidgetService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async getBotConfigService({
+    userId,
+    botId,
+  }: {
+    userId: string;
+    botId: string;
+  }) {
+    const foundBotConfig = this.prismaService.bots.findFirst({
+      where: {
+        bot_id: botId,
+        user_id: userId,
+      },
+    });
+
+    return foundBotConfig;
+  }
+}
